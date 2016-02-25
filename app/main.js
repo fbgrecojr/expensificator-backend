bf3f201bb3a674564462da407948f909276d657f
Shawn Petros <arashcuzi@gmail.com>
2016-02-25 10:12:35 -0600

    some sample endpoints and added removed CORS

--- app/main.js
+++ app/main.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/hello', function (req, res) {
+    res.json({
+        'this': 'that',
+        'something': 'something-else'
+    });
+});
+
+app.post('/poster', function (req, res) {
+    res.json({ 'cool': 'dude' }).status(200);
 });
 
 app.listen(3001, function () {
