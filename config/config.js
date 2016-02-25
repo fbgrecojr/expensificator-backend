55b1063f57df19feccec5944a756942276c2960b
keyushah <ku2u2001@gmail.com>
2016-02-25 11:36:26 -0600

    databaser

---
+++ config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    database: {
+        host: 'ds017258.mlab.com',
+        port: 17258,
+        db: 'expensificator',
+        username: 'admin',
+        password: 'admin'
+    },
+    server: {
+        port: 3001,
+        host: '127.0.0.1'
+    },
+    auth: {
+        key: 'thisisaverysecurekeybecauseitisalongkey'
+    }
+};
---
+++ config/database.js
@@ -0,0 +1,44 @@
+var Mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
+var config = require('./config');
+
+var options = {
+    server: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeOutMS: 30000
+        }
+    },
+    replset: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeOutMS: 30000
+        }
+    }
+};
+
+// var mongodburi = 'mongodb://user:password@host:port/db'
+//var mongodburi = 'mongodb://' +
+  //  config.database.username + ':' +
+   // config.database.password + '@' +
+    //config.database.host + ':' +
+    //config.database.port + '/' +
+    // config.database.db;
+
+ var mongodburi = 'mongodb://admin:admin@ds017258.mlab.com:17258/expensificator';   
+
+var mongooseUri = uriUtil.formatMongoose(mongodburi);
+
+Mongoose.connect(mongooseUri, options);
+
+var db = Mongoose.connection;
+
+db.on('error', console.error.bind('console', 'connection error'));
+
+db.once('open', function callback(){
+    console.log('database connected');
+});
+
+exports.Mongoose = Mongoose;
+
+exports.db = db;
