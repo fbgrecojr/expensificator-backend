5de3035eca3f303c7c7c03c6758e3eb55bda6f84
Robyn Welker <robynwelker@gmail.com>
2016-02-26 13:29:24 -0600

    cleaned up main

--- app/main.js
+++ app/main.js
@@ -5,30 +5,6 @@ var api = require('./routes/api');
 var database = require('./config/database');
 var bodyParser = require('body-parser');
 
-//<<<<<<< Updated upstream
-//require('../config/database');
-//var expense = require('../models/expense').expense;
-
-// var exp = require('../modules/createexpense');
-// var find = require('../modules/find');
-
-//some function
-
-// var request = {
-//   user : "user456",
-//   vendor: "Walmart" ,
-//   type :"dinner",
-//   Totalamount: 300,
-//   location : "India123",
-//   datePurchased: "1-1-2016",
-//   picture : "pcturebinarary"
-// };
-
-// exp(database.db, request);
-// find(database.db);
-
-//>>>>>>> Stashed changes
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -59,27 +35,6 @@ app.post('/poster', function (req, res) {
     res.json({ 'cool': 'dude' }).status(200);
 });
 
-/*app.post('/saveExpense', function(req, res){
-  var exp = new expense(req.body);
-
-  expense.save(function(error, data){
-    if(error){
-        res.json(error);
-    }
-    else{
-        res.json(data);
-    }
-  });
-});
-
-app.post('/findExpense', function(req,res){
-  var exp = new expense(req.body);
-
-  expense.find({type:'Groceries'}, function(error,data){
-    console.log(data);
-    res.json(data);
-  });
-}); */
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
